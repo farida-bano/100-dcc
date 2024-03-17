aa2c4e76ae6e0177333b3d8265794c42bcd4bb4f
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-17 23:23:02 +0500

    100 Days Coding Challenge
    Typescript

---
+++ index.js
@@ -0,0 +1,16 @@
+/*// Day :1 question:2
+let name1: string = "Asharib";
+// This saves the name
+
+console.log(`Hello ${name1}, would you like to learn some TypeScript today?`);
+// This shows the message*/
+//question :3
+let name = "Farida";
+//saves the name
+console.log(name.toLowerCase());
+//small letters
+console.log(name.toUpperCase());
+//BIG LETTERS
+console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+export {};
+// Shows the name with the First Letter Big
---
+++ index.ts
@@ -0,0 +1,15 @@
+/*// Day :1 question:2
+let name1: string = "Asharib";
+// This saves the name
+
+console.log(`Hello ${name1}, would you like to learn some TypeScript today?`);
+// This shows the message*/
+//question :3
+let name: string = "Farida"; 
+//saves the name
+console.log(name.toLowerCase()); 
+//small letters
+console.log(name.toUpperCase()); 
+//BIG LETTERS
+console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()); 
+// Shows the name with the First Letter Big
